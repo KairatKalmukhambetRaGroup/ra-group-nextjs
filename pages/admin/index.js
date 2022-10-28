9249b7c32453ae82b7bfea1f65aeca768845b57c
qashqyn <qashqyn291@gmail.com>
2022-10-28 22:34:35 +0600

    admin

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -48,48 +48,50 @@ export default function Admin() {
                 <title>RA Group | Admin panel</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="container">
-                <div className="semibold-28-32 text-center">Applications</div>
+            {/* <div className="container"> */}
+                <div className="content">
+                    <div className="semibold-32-48">Заявки на консультацию</div>
 
-                <table>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Fullname</th>
-                            <th>E-mail</th>
-                            <th>Company Name</th>
-                            <th>Format</th>
-                            <th>OS</th>
-                            <th>Language</th>
-                            <th>Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {applications && applications.map((application, key) => (
-                            <tr key={key}>
-                                <td>{key+1}</td>
-                                <td>{application.firstname} {application.lastname}</td>
-                                <td>
-                                    <a href={`mailto:${application.email}`}>
-                                        {application.email}
-                                    </a>
-                                </td>
-                                <td>{application.companyName}</td>
-                                <td>{application.format}</td>
-                                <td>
-                                    <div className="os-container">
-                                        {application.os && application.os.length>0 && application.os.map((oss, j)=> (
-                                            <div key={j} className="os">{oss}</div>
-                                        ))}
-                                    </div>
-                                </td>
-                                <td>{application.lang}</td>
-                                <td>{application.createdAt}</td>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Имя Фамилия</th>
+                                <th>Компания</th>
+                                <th>Электронная почта</th>
+                                <th>Формат</th>
+                                <th>ПО</th>
+                                <th>Язык</th>
+                                <th>Дата</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+                        </thead>
+                        <tbody>
+                            {applications && applications.map((application, key) => (
+                                <tr key={key}>
+                                    <td className="number">{key+1}</td>
+                                    <td>{application.firstname} {application.lastname}</td>
+                                    <td>{application.companyName}</td>
+                                    <td>
+                                        <a href={`mailto:${application.email}`}>
+                                            {application.email}
+                                        </a>
+                                    </td>
+                                    <td>{application.format}</td>
+                                    <td>
+                                        <div className="os-container">
+                                            {application.os && application.os.length>0 && application.os.map((oss, j)=> (
+                                                <div key={j} className="os">{oss}</div>
+                                            ))}
+                                        </div>
+                                    </td>
+                                    <td>{application.lang}</td>
+                                    <td>{application.createdAt}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            {/* </div> */}
         </div>
     );
 }
