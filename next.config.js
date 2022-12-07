08aa4624dfa7b32ee64a604c06d4e690b9e59d93
qashqyn <qashqyn291@gmail.com>
2022-12-07 08:05:29 +0600

    vcard redirect

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
     locales: ['en', 'ru', 'kz'],
     defaultLocale: 'en',
   },
+  async redirects() {
+    return [
+      {
+        // this will match `/english(default)/something` being requested
+        source: '/:slug(^[a-zA-z0-9]{8}$)',
+        destination: '/api/vcards/:slug',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig;
