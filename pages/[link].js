0c5aba36ce3cad2a062f60726e0de84153cd5123
qashqyn <qashqyn291@gmail.com>
2022-12-07 09:01:09 +0600

    vcard redirect 3

--- pages/[link].js
+++ pages/[link].js
@@ -6,6 +6,7 @@ const QRId = () => {
     const router = useRouter();
     const {link} = router.query;
 
+    
     // async function fetchVCard() {
     //     const data = await axios.get(`/api/qr/${link}`, {validateStatus: function (status) { return true }, headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"}});
         
@@ -13,26 +14,21 @@ const QRId = () => {
 
     useEffect(()=>{
         if(link){
-
-            // fetchVCard();
-            // var url = window.URL || window.webkitURL;
-            // link = url.createObjectURL(blob);
-            var a = document.createElement("a");
-            a.setAttribute("download", link);
-            a.setAttribute("href", `/api/vcards/${link}`);
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
-            window.close();
+            router.push(`/api/vcards/${link}`)
+            
+            // // fetchVCard();
+            // // var url = window.URL || window.webkitURL;
+            // // link = url.createObjectURL(blob);
+            // var a = document.createElement("a");
+            // a.setAttribute("download", link);
+            // a.setAttribute("href", `/api/vcards/${link}`);
+            // document.body.appendChild(a);
+            // a.click();
+            // document.body.removeChild(a);
+            // window.close();
         }
     },[link])
 
-    
-    return (
-        <div>
-            {link}
-        </div>
-    )
 }
 
 export default QRId;
