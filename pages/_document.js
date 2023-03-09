fc45c1d12442aee1b06bd7f6566aca0f7d572bfb
qashqyn <qashqyn291@gmail.com>
2023-03-09 11:44:50 +0600

    google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
@@ -27,10 +28,13 @@ class MyDocument extends Document {
                   <link rel="alternate" href={`${process.env.BASE_URL}`} hrefLang="en" />
                   <link rel="alternate" href={`${process.env.BASE_URL}/kz`} hrefLang="kz" />
                   <link rel="alternate" href={`${process.env.BASE_URL}/ru`} hrefLang="ru" />
+                  {/* Google Tag Manager */}
+                  <Script strategy='afterInteractive' dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PFKDT57');`}}></Script>
                 </Head>
                 <body>
-                <Main />
-                <NextScript />
+                  <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PFKDT57" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+                  <Main />
+                  <NextScript />
                 </body>
             </Html>
         );
