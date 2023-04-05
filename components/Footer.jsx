f8362afd24db13e3334abcefe0c8aafc2970a06b
qashqyn <qashqyn291@gmail.com>
2023-04-05 22:10:46 +0600

    phone number change

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = ({lang}) =>{
                     </a>
                     <div className="row">
                         <div className="col">
-                            <a href="tel:+77000882227" className="">+7 700 088 2227</a>
+                            <a href="tel:+770003002132" className="">+7 700 300 2132</a>
                             <div className="icons">
                                 <a href="https://wa.me/77000882227" target="_blank" className="icon whatsapp"><i></i></a>
                                 <a href="https://t.me/Bastatonaring" target="_blank" className="icon telegram"><i></i></a>
